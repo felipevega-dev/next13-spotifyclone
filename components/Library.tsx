333b36d9bd63e49f56f2b2b97928508fc730c790
Felipe Vega <feli.vega@duocuc.cl>
2023-06-07 17:21:48 -0400

    Completamente Free Use

--- components/Library.tsx
+++ components/Library.tsx
@@ -7,7 +7,6 @@ import { Song } from "@/types";
 import useUploadModal from "@/hooks/useUploadModal";
 import { useUser } from "@/hooks/useUser";
 import useAuthModal from "@/hooks/useAuthModal";
-import useSubscribeModal from "@/hooks/useSubscribeModal";
 import useOnPlay from "@/hooks/useOnPlay";
 
 import MediaItem from "./MediaItem";
@@ -19,20 +18,15 @@ interface LibraryProps {
 const Library: React.FC<LibraryProps> = ({
   songs
 }) => {
-  const { user, subscription } = useUser();
+  const { user } = useUser();
   const uploadModal = useUploadModal();
   const authModal = useAuthModal();
-  const subscribeModal = useSubscribeModal();
 
   const onPlay = useOnPlay(songs);
 
   const onClick = () => {
     if (!user) {
       return authModal.onOpen();
-    }
-
-    if (!subscription) {
-      return subscribeModal.onOpen();
     }
 
     return uploadModal.onOpen();
