f78868978d6032326005653127d8f1d0f3fa4fbb
Felipe Vega <feli.vega@duocuc.cl>
2023-06-28 16:51:26 -0400

    Suscripción eliminada

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,7 +1,7 @@
 import { User } from "@supabase/auth-helpers-nextjs";
 import { createContext, useState, useEffect, useContext } from "react";
 
-import { UserDetails , Subscription } from "@/types";
+import { UserDetails } from "@/types";
 import { useSessionContext , 
         useUser as useSupaUser
      } from "@supabase/auth-helpers-react";
@@ -12,7 +12,6 @@ type UserContextType = {
     user: User | null;
     userDetails: UserDetails | null;
     isLoading: boolean;
-    subscription: Subscription | null;
 };
 
 export const UserContext = createContext<UserContextType | undefined> (
@@ -35,21 +34,14 @@ export const MyUserContextProvider = (props: Props) => {
     const accessToken = session?.access_token ?? null;
     const [isLoadingData, setIsLoadingData] = useState(false);
     const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
-    const [subscription, setSubscription] = useState<Subscription | null>(null);
 
     const getUserDetails = () => supabase.from('users').select('*').single();
-    const getSubscription = () =>
-        supabase   
-            .from('subscriptions')
-            .select('*, prices(*, products(*))')
-            .in('status', ['trialing', 'active'])
-            .single();
 
     useEffect(() => {
-        if (user && !isLoadingData && !userDetails && !subscription ) {
+        if (user && !isLoadingData && !userDetails) {
             setIsLoadingData(true);
 
-            Promise.allSettled([getUserDetails(), getSubscription()]).then(
+            Promise.allSettled([getUserDetails()]).then(
                 (results) => {
                     const userDetailsPromise = results [0];
                     const subscriptionPromise = results [1];
@@ -58,16 +50,11 @@ export const MyUserContextProvider = (props: Props) => {
                         setUserDetails(userDetailsPromise.value.data as UserDetails);
                     }
 
-                    if (subscriptionPromise.status === "fulfilled"){
-                        setSubscription(subscriptionPromise.value.data as Subscription);
-                    }
-
                     setIsLoadingData(false);
                 }
             );
         } else if (!user && !isLoadingUser && !isLoadingData) {
             setUserDetails(null);
-            setSubscription(null);
         }
     }, [user, isLoadingUser]);
 
@@ -76,7 +63,6 @@ export const MyUserContextProvider = (props: Props) => {
         user,
         userDetails,
         isLoading: isLoadingUser || isLoadingData,
-        subscription
     };
 
     return <UserContext.Provider value={value} {...props}/>
--- types.ts
+++ types.ts
@@ -42,21 +42,3 @@ export interface Price{
     metadata?: Stripe.Metadata;
     products?: Product;
 }
-
-export interface Subscription {
-    id: string;
-    user_id: string;
-    status?: Stripe.Subscription.Status;
-    metadata?: Stripe.Metadata;
-    price_id?: string;
-    quantity?: number;
-    cancel_at_period_end?: boolean;
-    created: string;
-    current_period_start: string;
-    current_period_end: string;
-    ended_at?: string;
-    cancel_at?: string;
-    canceled_at?: string;
-    trial_start?: string;
-    prices?: Price;
-}
